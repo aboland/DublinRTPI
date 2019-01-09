dfb7e0a094de3e95c28a43bc0d2aac2bfd1d07b0
aboland <aidan.boland@ucd.ie>
2019-01-09 00:00:31 +0000

    small sample script calling dublin bus api

---
+++ testing_functions.R
@@ -0,0 +1,40 @@
+
+
+#
+#  https://data.smartdublin.ie/dataset/real-time-passenger-information-rtpi-for-dublin-bus-bus-eireann-luas-and-irish-rail
+#
+#  http://api.irishrail.ie/realtime/index.htm?realtime_irishrail
+#
+#
+
+
+
+library(jsonlite)
+library(tidyverse)
+
+
+my_stop <- jsonlite::fromJSON("https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=334&format=json")
+
+
+short_my_stop <- my_stop$results %>% select(duetime, departureduetime, destination, route, monitored, sourcetimestamp)
+  
+short_my_stop %>% filter(route == 140)
+
+  
+get_stop_info <- function(stop_number, selected_route = NULL){
+  
+  if(!is.null(selected_route)){
+    all_info <- jsonlite::fromJSON(paste0("https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=", stop_number,"&routeid=",selected_route,"&format=json"))
+  }else{
+    all_info <- jsonlite::fromJSON(paste0("https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=", stop_number,"&format=json"))
+  }
+  
+  if(all_info$errorcode == 0){
+    tidy_info <- all_info$results %>% select(duetime, departureduetime, destination, route, monitored, sourcetimestamp)
+  }else{
+    tidy_info <- "No info"
+  }
+  return(tidy_info)
+}
+
+get_stop_info(334, 140)
