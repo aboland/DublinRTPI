1f6d75c4b7c66229dc46d9813f1df2b3f44b0d12
aboland <aidan.boland@ucd.ie>
2019-02-26 20:29:33 +0000

    Fixed bug and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DublinRTPI
 Real-time Passenger Information for Dublin city's public transport
+
+
+### Run Shiny App locally
+
+`library(shiny)`
+`runGitHub("aboland/DublinRTPI", subdir = "ShinyApp")`
--- ShinyApp/helpers.R
+++ ShinyApp/helpers.R
@@ -53,19 +53,25 @@ db_get_multi_stop_info <- function(stop_numbers){
 
 db_scrape_stop_route_info <- function(stop_number){
   
-  test_webpage <- read_html(paste0("http://dublinbus.ie/en/RTPI/Sources-of-Real-Time-Information/?searchtype=view&searchquery=", stop_number))
+  webpage_data <- read_html(paste0("http://dublinbus.ie/en/RTPI/Sources-of-Real-Time-Information/?searchtype=view&searchquery=", stop_number))
   
-  all_text <- test_webpage  %>% html_nodes('#real-time-display') %>% html_text()
+  all_text <- webpage_data  %>% html_nodes('#real-time-display') %>% html_text()
+  
   if(length(all_text)>0){
     tidy_text <- all_text %>% str_split("\r\n") %>% unlist() %>% str_trim()
     
     start_text <- which(tidy_text == "Route")
     end_text <- which(tidy_text=="Accessible")
     
-    useful_info <- tidy_text[start_text:(end_text-1)]
-    useful_info <- useful_info[which(useful_info!="" & useful_info != "Notes")]
-    useful_table <- matrix(useful_info[-(1:3)], ncol=3, byrow = T, dimnames = list(NULL, useful_info[1:3]))
-    return(list(results = as_data_frame(useful_table), errorcode = 0))
+    if(length(start_text)>0){
+      useful_info <- tidy_text[start_text:(end_text-1)]
+      useful_info <- useful_info[which(useful_info!="" & useful_info != "Notes")]
+      useful_table <- matrix(useful_info[-(1:3)], ncol=3, byrow = T, dimnames = list(NULL, useful_info[1:3]))
+    
+      return(list(results = as_data_frame(useful_table), errorcode = 0))
+    }else{
+      return(list(errorcode = 1, errormessage = "No bus times available"))
+    }
   }else{
     return(list(errorcode = 1, errormessage = "No results found"))
   }
